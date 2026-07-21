6b49c6d51b6e7556ed768a926cf518d5f29e6217
michael.swanson <70359078+33mswan@users.noreply.github.com>
2026-07-20 21:18:31 -0600

    Add hash router and veteran profile view

--- app.js
+++ app.js
@@ -23,8 +23,42 @@ function renderDirectory() {
   });
 }
 
-function router() {
-  renderDirectory();
+function showView(viewId) {
+  document.querySelectorAll(".view").forEach(function (view) {
+    view.hidden = view.id !== viewId;
+  });
 }
 
+function renderProfile(vet) {
+  const content = document.getElementById("profile-content");
+  content.innerHTML =
+    '<div class="avatar avatar-large">' + vet.avatarInitials + '</div>' +
+    '<h2 class="veteran-name">' + vet.name + '</h2>' +
+    '<p class="veteran-meta">' + vet.branch + ' &middot; ' + vet.era + '</p>' +
+    '<p class="veteran-bio">' + vet.bio + '</p>' +
+    '<p class="veteran-total" id="profile-total">' + formatCurrency(vet.totalTipped) + ' tipped</p>';
+}
+
+function router() {
+  const hash = window.location.hash;
+  const profileMatch = hash.match(/^#\/vets\/(.+)$/);
+
+  if (profileMatch) {
+    const vet = findVeteran(profileMatch[1]);
+    if (vet) {
+      renderProfile(vet);
+      showView("profile-view");
+      return;
+    }
+  }
+
+  renderDirectory();
+  showView("directory-view");
+}
+
+document.getElementById("back-button").addEventListener("click", function () {
+  window.location.hash = "#/";
+});
+
+window.addEventListener("hashchange", router);
 window.addEventListener("DOMContentLoaded", router);
